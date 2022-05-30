bbff367ddb5c47919c13c86ba979cd3607302438
lucas-amaral <lucasa2a2@hotmail.com>
2022-05-30 15:50:36 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,17 +12,17 @@ set -eu
 #     esac
 # done
 
-set user="$1"
-set password="$2"
-set jira_base_url="$3"
-set message="$4"
+# set user="$1"
+# set password="$2"
+# set jira_base_url="$3"
+# set message="$4"
 
-if [[ "$message" =~ [a-zA-Z0-9]+-[0-9]+ ]]; then
-   keys=$(echo "$message" | grep -P '[a-zA-Z0-9]+-[0-9]+' -o)
+if [[ $MESSAGE =~ [a-zA-Z0-9]+-[0-9]+ ]]; then
+   keys=$(echo $MESSAGE | grep -P '[a-zA-Z0-9]+-[0-9]+' -o)
    while IFS='/| ' read -ra KEYS; do
      for issueKey in "${KEYS[@]}"; do
        echo "Run search for: $issueKey"
-       curl -D- -u "$user":"$password" -H "Content-Type: application/json" "$jira_base_url/rest/api/2/issue/picker?query=$issueKey" -o findjirakey.json             
+       curl -D- -u $JIRA_USER:$JIRA_PASSWORD -H "Content-Type: application/json" "$JIRA_BASE_URL/rest/api/2/issue/picker?query=$issueKey" -o findjirakey.json             
        key=$(cat findjirakey.json | jq -c '.sections[0].issues[0].key')
        if [ "$key" != null ]; then
            echo "Jira key found: $issueKey"
