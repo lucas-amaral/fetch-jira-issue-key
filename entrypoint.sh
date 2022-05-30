ebe0998a20a98876cb0b3cae36999ffa753f3809
lucas-amaral <lucasa2a2@hotmail.com>
2022-05-30 17:26:41 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
 function get_jira_ticket() {
+    issueKey="$1"
     curl -D- -u $JIRA_USER:$JIRA_PASSWORD -H "Content-Type: application/json" "$JIRA_BASE_URL/rest/api/2/issue/picker?query=$issueKey" -o findjirakey.json
 }
 
 function set_issue_key_if_exist() {
-   issueKey="$1"
-   key=$(cat findjirakey.json | jq -c '.sections[0].issues[0].key')
-   if [ "$key" != null ]; then
-     echo "Jira key found: $issueKey"
-     echo "::set-output name=issue::$issueKey"
-     exit
-   fi
+    issueKey="$1"
+    key=$(cat findjirakey.json | jq -c '.sections[0].issues[0].key')
+    if [ "$key" != null ]; then
+        echo "Jira key found: $issueKey"
+        echo "::set-output name=issue::$issueKey"
+        exit
+    fi
 }
 
 
@@ -20,7 +21,7 @@ if [[ $MESSAGE =~ [a-zA-Z0-9]+-[0-9]+ ]]; then
    while IFS='/| ' read -ra KEYS; do
      for issueKey in "${KEYS[@]}"; do
        echo "Run search for: $issueKey"
-       $(get_jira_ticket)
+       $(get_jira_ticket $issueKey)
        $(set_issue_key_if_exist $issueKey)
      done
    done <<< "$keys"
